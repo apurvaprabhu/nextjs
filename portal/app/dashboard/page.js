f1021d969475d3b5dfa7ced862d567d4d18832dd
Apurva <apurvaprahu80@gmail.com>
2024-01-06 01:31:46 +0530

    wip

--- portal/app/dashboard/page.js
+++ portal/app/dashboard/page.js
@@ -16,23 +16,34 @@ export default function Home() {
                 <div className="text-center">   
                 <Image src='/students.jpg'  width={400}
                 height={300}></Image>
+                  <h4 className="mt-1 mb-5 pb-1">A decentralized application to upload and verfy certificates on Ethereum blockchain</h4>
                 </div>
               </div>
-            </div>       
+            </div>
             <div className="col-lg-6 d-flex align-items-center gradient-custom-2">
-               <div className="row g-0"> <h1>Temper proof document</h1></div>
-               
+            <div className="text-center">   
+                  <h4 className="mt-1 mb-5 pb-1">Choose your Account</h4>
+                </div>
+                
 <div className="text-center">
-       <Link href="/dashboard" replace>
-           <button type="button" className="btn btn-lg btn-outline-primary m-2">Dashboard
+       <Link href="/student-login" replace>
+           <button type="button" className="btn btn-lg btn-outline-primary m-2">Student
 </button>
     </Link>
+    <Link href="/admin-login" replace>
+    <button type="button" className="btn btn-lg btn-outline-primary m-2">Institution
+</button>
+    </Link>
+    <Link href="/admin-login" replace>
+    <button type="button" className="btn btn-lg btn-outline-primary m-2">Company
+</button>
+</Link>
+</div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
   </div>
     </main>
     )
